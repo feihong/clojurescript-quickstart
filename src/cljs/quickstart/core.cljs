c8405de20217994a636330fdf731332e5d8287b4
Feihong Hsu <feihong.hsu@gmail.com>
2018-01-05 14:47:44 -0600

    Add button to generate random emoji

--- src/cljs/quickstart/core.cljs
+++ src/cljs/quickstart/core.cljs
@@ -1,6 +1,12 @@
 (ns quickstart.core)
 
-(enable-console-print!)
+(defn set-text [elem-id text]
+  (let [el (js/document.getElementById elem-id)]
+    (set! (.-textContent el) text)))
+
+(defn on-click [btn-id func]
+  (let [el (js/document.getElementById btn-id)]
+    (set! (.-onclick el) func)))
 
 (defn random-hanzi []
   (let [start 0x4e00
@@ -14,11 +20,36 @@
     hanzi))
 
 (defn set-hanzi []
-  (let [hanzi (random-hanzi)
-        el (js/document.getElementById "hanzi")]
-    (set! (.-textContent el) hanzi)))
+  (set-text "hanzi" (random-hanzi)))
 
-(let [btn (js/document.getElementById "generate-btn")]
-  (set! (.-onclick btn) set-hanzi))
+(def shortnames (-> (.-shortnames js/emojione)
+                    (clojure.string/split "|")))
+
+(defn get-emoji [shortname]
+  (let [div (js/document.createElement "div")
+        html (.shortnameToImage js/emojione shortname)]
+    (set! (.-innerHTML div) html)
+    (let [img (.-firstChild div)]
+      {:text (.-alt img)
+       :shortname (.-title img)
+       :url (.-src img)})))
+
+(defn random-emoji []
+  (let [emoji (-> (rand-nth shortnames) get-emoji)]
+    (println "Random emoji" emoji)
+    emoji))
+
+(defn set-emoji []
+  (let [text (-> (random-emoji) :text)]
+    (set-text "emoji" text)))
+
+;; Initialization
+(enable-console-print!)
+
+(on-click "hanzi-btn" set-hanzi)
+(on-click "emoji-btn" set-emoji)
 
 (set-hanzi)
+(set-emoji)
+
+(set! (.-imagePathPNG js/emojione) "https://cdn.jsdelivr.net/emojione/assets/3.1/png/128/")
